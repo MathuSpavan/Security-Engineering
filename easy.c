6d772492fba65ce438956d876ac22e080e4a7767
mathusan <matu@mail.de>
2025-04-14 18:23:43 +0200

    test push

---
+++ easy.c
@@ -0,0 +1,12 @@
+//
+// Created by Mathusan Saravanapavan on 14.04.25.
+//
+
+#include "easy.h"
+#include <stdio.h>
+
+int main()
+  {
+  printf("Hello world!\n");
+
+}
---
+++ easy.h
@@ -0,0 +1,8 @@
+//
+// Created by Mathusan Saravanapavan on 14.04.25.
+//
+
+#ifndef EASY_H
+#define EASY_H
+
+#endif //EASY_H
